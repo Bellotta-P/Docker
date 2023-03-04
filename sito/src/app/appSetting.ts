75b4edacd9a07b1fd88ae613bd3b3c0f4ea91af1
Bellotta-P <bellotta.pietro@iisgalvanimi.edu.it>
2023-03-04 15:16:40 +0000

    modifiche

--- sito/src/app/appSetting.ts
+++ sito/src/app/appSetting.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-    public static _API  ='https://5000-bellottap-docker-xxzgx1c2n0s.ws-eu89.gitpod.io/';
-   // public static _API = ('/api/');
+    //public static _API  ='https://5000-bellottap-docker-xxzgx1c2n0s.ws-eu89.gitpod.io/';
+    public static _API = ('/api/');
 
  }
